124de92ac275b78e3f50d2f294c8ec4b3e4e9e64
pawan1773 <joginder.pawan@gmail.com>
2019-11-17 20:23:18 +0530

    currency exchange

---
+++ currency-exchange-service/src/main/java/com/learning/microservices/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,24 @@
+package com.learning.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+	// related to sleuth
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
+}
